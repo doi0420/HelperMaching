0848047773bc5a73ef3bb007eeba7173273e0bb9
Apple <apple@ApplenoMacBook-Pro.local>
2021-02-22 03:46:22 +0900

    修正テスト２９

--- main.py
+++ main.py
@@ -1,67 +1,48 @@
-
 from flask import Flask, request
 import random, json, requests
 import pandas as pd
 import random
-import os
-import sys
+
 
 # line libray
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, StickerSendMessage,ImageSendMessage)
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage,ImageSendMessage
+)
 
-#ユーザ区分[0:タクシー会社、1:依頼者]
-dicUsrKbn={'U409026962871bf8786172850baa56f62':0, 'U7bb673b5d4a90c19698ef689b421985e':1}
-#申請状況を管理[0:配車依頼中、1:オプション選択中、2:車イス対応返信待ち、
-#　　　　　　　 3:ストレッチャー対応返信待ち、4:マイクロバス返信街、9:現在処理中]
-dicStatus={}
+import os
 
-#名前を取得する
-dicGetName={}
+VehicleDispatchFg=0
+VehicleDispatchKind=0
 
-#タクシー会社の返信状況を管理[0:未処理、9:処理中]
-dicTaxiStatus={'U409026962871bf8786172850baa56f62':0}
+def VehicleDispatchFg_now():
+    global VehicleDispatchFg
+    return VehicleDispatchFg
 
-#メッセージを返信します。
-def replyMessage(event,msg):
-    line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=msg)
-    )
+def VehicleDispatchCheck():
+    Fg = VehicleDispatchFg_now()
+    if Fg ==1:
+        return True
+    else:
+        return False
 
-#依頼者へメッセージを返信します。
-def pushMessage(userId,msg):
-    messages = TextSendMessage(text=msg)
-    line_bot_api.push_message(userId, messages=messages)
+def SetElseStr():
+    wkStr = ""
+    if VehicleDispatchKind == 0:
+        wkStr = "現在は配車のオプション選択待ちです。\n１：車イス対応\n２：ストレッチャー対応\n３：マイクロバス\nからオプションをコメントして下さい。"
+    elif VehicleDispatchKind == 1:
+        wkStr = "現在はアイネット交通株式会社へ配車依頼中です。\nもうしばらくお待ち下さい。"
+    elif VehicleDispatchKind == 2:
+        wkStr = "現在はINET交通　株式会社へ配車依頼中です。\nもうしばらくお待ち下さい。"
+    elif VehicleDispatchKind == 3:
+        wkStr = "現在はあいねっと交通株式会社へ配車依頼中です。\nもうしばらくお待ち下さい。"
 
-#タクシー会社の受つけ状況を返信します
-def replyTaxiMessage(key,event):
-    global dicGetName
-    global dicStatus
-    retcnt = 0
-    if dicStatus[key]==2:
-        #依頼者へ受け付けた旨を返信する
-        pushMessage(key,"おまたせ致しました。\nアイネット交通株式会社からの配車が確定しました。")
-        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：車イス対応タクシー")
-        #タクシー会社へマッチングした旨を返信する
-        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-        retcnt = 1
-    elif dicStatus[key]==3:
-        #依頼者へ受け付けた旨を返信する
-        pushMessage(key,"おまたせ致しました。\nINET交通　株式会社からの配車が確定しました。")
-        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：ストレッチャー対応タクシー")
-        #タクシー会社へマッチングした旨を返信する
-        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-        retcnt = 1
-    elif dicStatus[key]==4:
-        #依頼者へ受け付けた旨を返信する
-        pushMessage(key,"おまたせ致しました。\nあいねっと交通株式会社からの配車が確定しました。")
-        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：マイクロバス")
-        #タクシー会社へマッチングした旨を返信する
-        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-        retcnt = 1
-    return retcnt
+    return wkStr
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET       = os.environ["YOUR_CHANNEL_SECRET"]
@@ -94,70 +75,137 @@ def callback():
 # メッセージ応答メソッド
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "ユーザ":
-        replyMessage(event,event.source.user_id)
+    USER_ID = "U7bb673b5d4a90c19698ef689b421985e"
+    KAIGO_USER_ID = "U409026962871bf8786172850baa56f62"
+    global VehicleDispatchFg
+    global VehicleDispatchKind
+    #　メッセージは "event.message.text" という変数に格納される
+    if event.source.user_id == USER_ID and event.message.text =="1":
+        wkStr1 = ""
+        wkStr2 = ""
+        if VehicleDispatchKind == 1:
+            wkStr1 = "おまたせ致しました。\nアイネット交通株式会社からの配車が確定しました。"
+            wkStr2 = "到着地：東京都大田区蒲田5-37-1\n車種：車イス対応タクシー"
+            messages = TextSendMessage(text=wkStr1)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
+            messages = TextSendMessage(text=wkStr2)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
+        elif VehicleDispatchKind == 2:
+            wkStr1 = "おまたせ致しました。\nINET交通　株式会社からの配車が確定しました。"
+            wkStr2 = "到着地：東京都大田区蒲田5-37-1\n車種：ストレッチャー対応タクシー"
+            messages = TextSendMessage(text=wkStr1)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
+            messages = TextSendMessage(text=wkStr2)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
+        elif VehicleDispatchKind == 3:
+            wkStr1 = "おまたせ致しました。\nあいねっと交通株式会社からの配車が確定しました。"
+            wkStr2 = "到着地：東京都大田区蒲田5-37-1\n車種：マイクロバス"
+            messages = TextSendMessage(text=wkStr1)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
+            messages = TextSendMessage(text=wkStr2)
+            line_bot_api.push_message("U96671e7042e3f7fc2efee15b6c7f840f", messages=messages)
 
-    #グローバル変数
-    global dicStatus
-    global dicGetName
-    global dicUsrKbn
+        VehicleDispatchKind=0
+        VehicleDispatchFg=0
 
-    #リクエストのあったユーザの区分を取得
-    usrKbn=dicUsrKbn.get(event.source.user_id,-1)
-    #ディクショナリーになかったときは、依頼者として追加する
-    if usrKbn==-1:
-        dicUsrKbn[event.source.user_id]=1
-        dicStatus[event.source.user_id]=0
-        pushMessage('U409026962871bf8786172850baa56f62',str(dicStatus[event.source.user_id]))
- 
-    #現在タクシー会社が処理中ではないことを確認する。
-    if dicTaxiStatus[event.source.user_id]==9:
-        replyMessage(event,"現在処理中です。")
-        sys.exit()
+    elif event.source.user_id == USER_ID and event.message.text =="2":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="申し訳ございません。現在対応可能なタクシーはございません。\nしばらくして再度申し込み下さい。")
+        )
+        VehicleDispatchKind=0
+        VehicleDispatchFg=0  
+    elif event.message.text == "配車依頼" and VehicleDispatchCheck()==False:
+        VehicleDispatchStr1 = "配車を手配致します。"
+        VehicleDispatchStr2 = "ご希望の車種を番号でご選択下さい。\n１：車イス対応\n２：ストレッチャー対応\n３：マイクロバス"
+        VehicleDispatchFg = 1
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text=VehicleDispatchStr1),
+                TextSendMessage(text=VehicleDispatchStr2)
+            ]
+        )
+    elif VehicleDispatchCheck():
+        profile = line_bot_api.get_profile(event.source.user_id)
 
-    #依頼者の場合
-    pushMessage('U409026962871bf8786172850baa56f62',event.message.text)   
-    if event.message.text == '配車依頼':
-        pushMessage('U409026962871bf8786172850baa56f62',str(dicStatus[event.source.user_id]))
-        #申請状況に応じてメッセージを返す
-        #配車依頼中の場合
-        if dicStatus[event.source.user_id] ==0:
-            pushMessage('U409026962871bf8786172850baa56f62',"依頼者最初")
-            if event.message.text == "配車依頼":
-                dicStatus[event.source.user_id] =1
-                replyMessage(event,"現在は配車のオプション選択待ちです。\n１：車イス対応\n２：ストレッチャー対応\n３：マイクロバス\nからオプションをコメントして下さい。")
-            else:
-                del dicStatus[event.source.user_id]
-        #オプション選択中の場合
-        elif dicStatus[event.source.user_id] ==1:
-            profile = line_bot_api.get_profile(event.source.user_id)
-            if event.message.text == "1":
-                dicStatus[event.source.user_id] =2
-                pushMessage('U409026962871bf8786172850baa56f62',profile.display_name + "様から配車依頼がありました。\n車イス対応車を希望です。")
-                replyMessage(event, "現在はINET交通　株式会社へ配車依頼中です。\nもうしばらくお待ち下さい。")
-    #タクシー会社の場合
-    if dicUsrKbn[event.source.user_id]==0:
-        replycnt = 0
-        #依頼者に申請中のステータスがある場合、配車を受け付けた旨を返信する。
         if event.message.text == "1":
-            for key in dicStatus:
-                replycnt += replyTaxiMessage(key,event)
-        #依頼者に申請中のステータスがある場合、配車を受け付けられなかった旨を返信する。
+            VehicleDispatchKind = 1
+            messages = TextSendMessage(text=profile.display_name + "様から配車依頼がありました。\n車イス対応車を希望です。")
+            line_bot_api.push_message(USER_ID, messages=messages)
+            messages = TextSendMessage(text="対応可否を番号でご選択下さい。\n１：対応可能\n２：対応不可")
+            line_bot_api.push_message(USER_ID, messages=messages)
+
+            text = "アイネット交通株式会社へ依頼中です。\nしばらくお待ち下さい。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text)
+            )
         elif event.message.text == "2":
-            for key in dicStatus:
-                if dicStatus[key]==2 or dicStatus[key]==3 or dicStatus[key]==4:
-                    #対応不可のメッセージを依頼者へ返信する
-                    pushMessage(key,"申し訳ございません。現在対応可能なタクシー会社はございません。")
-                    #申請状況ディクショナリーからキーを削除する
-                    del dicStatus[key]
-                    replycnt = 1
+            VehicleDispatchKind = 2
+            messages = TextSendMessage(text=profile.display_name + "様から配車依頼がありました。\nストレッチャー対応車を希望です。")
+            line_bot_api.push_message(USER_ID, messages=messages)
+            messages = TextSendMessage(text="対応可否を番号でご選択下さい。\n１：対応可能\n２：対応不可")
+            line_bot_api.push_message(USER_ID, messages=messages)            
+            text = "INET交通　株式会社へ依頼中です。\nしばらくお待ち下さい。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text)
+            )
+        elif event.message.text == "3":
+            VehicleDispatchKind = 3
+            messages = TextSendMessage(text=profile.display_name + "様から配車依頼がありました。\nマイクロバスを希望です。")
+            line_bot_api.push_message(USER_ID, messages=messages)
+            messages = TextSendMessage(text="対応可否を番号でご選択下さい。\n１：対応可能\n２：対応不可")
+            line_bot_api.push_message(USER_ID, messages=messages)    
+            text = "あいねっと交通株式会社へ依頼中です。\nしばらくお待ち下さい。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text)
+            )
+        elif event.message.text == "タクシー会社":
+            TaxiListStr1 = "対応可能なタクシー会社です。"
+            TaxiListStr2 = "https://www.taxisite.com/station/info/9931003.aspx"
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=TaxiListStr1),
+                    TextSendMessage(text=TaxiListStr2)
+                ]
+            )
+        elif event.message.text == "キャンセル":
+            text = "配車の手配をキャンセルしました。\nまたのご利用をお待ちしております。"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text)
+            )
+            VehicleDispatchFg = 0
+            VehicleDispatchKind = 0
         else:
-            replyMessage(event,'このボタンは受け付けていません') 
-
-        #一つでも返信があった場合と一つも返信がなかった場合で処理を分ける
-        if replycnt == 0:
-            replyMessage(event,'現在返信待ちの依頼はありませんでした')   
-
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=SetElseStr())
+            )        
+    elif event.message.text == "タクシー会社":
+        TaxiListStr1 = "対応可能なタクシー会社です。"
+        TaxiListStr2 = "https://www.taxisite.com/station/info/9931003.aspx"
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text=TaxiListStr1),
+                TextSendMessage(text=TaxiListStr2)
+            ]
+        )
+    elif event.message.text == "あ":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.source.user_id)
+            )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="現在は何も受付おりません。")
+            )
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
@@ -168,5 +216,3 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 8080))
     )
-# [END gae_python37_app]
-
