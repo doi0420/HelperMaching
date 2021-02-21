4afd0d1d8359246b4bace941bfdfd6879cd28cf8
Apple <apple@ApplenoMacBook-Pro.local>
2021-02-21 18:45:02 +0900

    修正テスト７

--- main.py
+++ main.py
@@ -36,6 +36,32 @@ def pushMessage(userId,msg):
     messages = TextSendMessage(text=msg)
     line_bot_api.push_message(userId, messages=messages)
 
+#タクシー会社の受つけ状況を返信します
+def replyTaxiMessage(key,event,repFg):
+    global dicGetName
+    global dicStatus
+    if dicStatus[key]==2:
+        #依頼者へ受け付けた旨を返信する
+        pushMessage(key,"おまたせ致しました。\nアイネット交通株式会社からの配車が確定しました。")
+        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：車イス対応タクシー")
+        #タクシー会社へマッチングした旨を返信する
+        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
+        repFG = True
+    elif dicStatus[key]==3:
+        #依頼者へ受け付けた旨を返信する
+        pushMessage(key,"おまたせ致しました。\nINET交通　株式会社からの配車が確定しました。")
+        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：ストレッチャー対応タクシー")
+        #タクシー会社へマッチングした旨を返信する
+        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
+        repFG = True
+    elif dicStatus[key]==4:
+        #依頼者へ受け付けた旨を返信する
+        pushMessage(key,"おまたせ致しました。\nあいねっと交通株式会社からの配車が確定しました。")
+        pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：マイクロバス")
+        #タクシー会社へマッチングした旨を返信する
+        replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
+        repFG = True
+    return repFG
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET       = os.environ["YOUR_CHANNEL_SECRET"]
@@ -71,6 +97,11 @@ def handle_message(event):
     if event.message.text == "ユーザ":
         replyMessage(event,event.source.user_id)
 
+    #グローバル変数
+    global dicStatus
+    global dicGetName
+    global dicUsrKbn
+
     #リクエストのあったユーザの区分を取得
     usrKbn=dicUsrKbn.get(event.source.user_id,-1)
     #ディクショナリーになかったときは、依頼者として追加する
@@ -89,28 +120,7 @@ def handle_message(event):
         #依頼者に申請中のステータスがある場合、配車を受け付けた旨を返信する。
         if event.message.text == "1":
             for key in dicStatus:
-                if dicStatus[key]==2:
-                    #依頼者へ受け付けた旨を返信する
-                    pushMessage(key,"おまたせ致しました。\nアイネット交通株式会社からの配車が確定しました。")
-                    pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：車イス対応タクシー")
-                    #タクシー会社へマッチングした旨を返信する
-                    replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-                    replyFG = True
-                elif dicStatus[key]==3:
-                    #依頼者へ受け付けた旨を返信する
-                    pushMessage(key,"おまたせ致しました。\nINET交通　株式会社からの配車が確定しました。")
-                    pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：ストレッチャー対応タクシー")
-                    #タクシー会社へマッチングした旨を返信する
-                    replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-                    replyFG = True
-                elif dicStatus[key]==4:
-                    #依頼者へ受け付けた旨を返信する
-                    pushMessage(key,"おまたせ致しました。\nあいねっと交通株式会社からの配車が確定しました。")
-                    pushMessage(key,"到着地：東京都大田区蒲田5-37-1\n車種：マイクロバス")
-                    #タクシー会社へマッチングした旨を返信する
-                    replyMessage(event,dicGetName[key] + '様の配車依頼を受付ました。')
-                    replyFG = True
-
+                replyFG = replyTaxiMessage(key,event,replyFG)
         #依頼者に申請中のステータスがある場合、配車を受け付けられなかった旨を返信する。
         elif event.message.text == "2":
             for key in dicStatus:
@@ -123,10 +133,31 @@ def handle_message(event):
         else:
             replyMessage(event,'このボタンは受け付けていません') 
             sys.exit()
-            
+
         #一つでも返信があった場合と一つも返信がなかった場合で処理を分ける
         if replyFG==False:
             replyMessage(event,'現在返信待ちの依頼はありませんでした')           
+    #依頼者の場合
+    if usrKbn ==1:
+        #申請状況に応じてメッセージを返す
+        #配車依頼中の場合
+        if dicStatus[event.source.user_id] ==0:
+            if event.message.text == "配車依頼":
+                dicStatus[event.source.user_id] =1
+                replyMessage(event,"現在は配車のオプション選択待ちです。\n１：車イス対応\n２：ストレッチャー対応\n３：マイクロバス\nからオプションをコメントして下さい。")
+            else:
+                del dicStatus[key]
+        #オプション選択中の場合
+        elif dicStatus[event.source.user_id] ==1:
+            profile = line_bot_api.get_profile(event.source.user_id)
+            if event.message.text == "1":
+                dicStatus[event.source.user_id] =2
+                pushMessage('U409026962871bf8786172850baa56f62',profile.display_name + "様から配車依頼がありました。\n車イス対応車を希望です。")
+                replyMessage(event, "現在はINET交通　株式会社へ配車依頼中です。\nもうしばらくお待ち下さい。")
+                          
+
+
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
